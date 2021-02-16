0c813db4e29d238f17b96ba0cf4f82717d31d249
JustYarka <42116658+JustYarka@users.noreply.github.com>
2021-02-16 09:51:22 +0300

    Russian translation

--- gateways/unitpay.php
+++ gateways/unitpay.php
@@ -1,20 +1,20 @@
 <?php
 function unitpay_config() {
     $configarray = array(
-     "FriendlyName" => array("Type" => "System", "Value"=>"UnitPay"),
-     "DOMAIN" => array("FriendlyName" => "Domain name", "Type" => "text", "Size" => "60", "Description" => "Your working domain" ),
-     "URL" => array("FriendlyName" => "Payment Form URL", "Type" => "text", "Size" => "60", "Description" => "(Example: https://domain_name/pay/<b>262fde297f8e4e3d31e272d74aa39401</b>)" ),
-     "SecretKey" => array("FriendlyName" => "Secret Key", "Type" => "text", "Size" => "60", ),
-     "hideHint" => array("FriendlyName" => "Hide Hint", "Type" => "yesno","Description" => ""),
-     "hideBackUrl" => array("FriendlyName" => "Hide Shop Url", "Type" => "yesno","Description" => ""),
-     "hideOrderCost" => array("FriendlyName" => "Hide Order Cost", "Type" => "yesno","Description" => ""),
-     "hideLogo" => array("FriendlyName" => "Hide UnitPay Logo", "Type" => "yesno","Description" => "")
+        "FriendlyName" => array("Type" => "System", "Value"=>"UnitPay"),
+        "DOMAIN" => array("FriendlyName" => "Домен", "Type" => "text", "Size" => "60", "Description" => "<b>Подсказка:</b> Домен платежки (unitpay.money) [Добавлен, если касса вдруг изменит домен]" ),
+        "URL" => array("FriendlyName" => "Ссылка на форму оплаты", "Type" => "text", "Size" => "60", "Description" => "<b>Подсказка:</b> https://domain_name/pay/<b>262fde297f8e4e3d31e272d74aa39401</b>" ),
+        "SecretKey" => array("FriendlyName" => "Секретный ключ", "Type" => "text", "Size" => "60", "Description" => "<b>Подсказка:</b> В настройках проекта UnitPay"),
+        "hideHint" => array("FriendlyName" => "Скрывать подсказки", "Type" => "yesno","Description" => ""),
+        "hideBackUrl" => array("FriendlyName" => "Скрывать ссылку на магазин", "Type" => "yesno","Description" => ""),
+        "hideOrderCost" => array("FriendlyName" => "Скрывать стоимость заказа", "Type" => "yesno","Description" => ""),
+        "hideLogo" => array("FriendlyName" => "Скрывать логотип UnitPay", "Type" => "yesno","Description" => "")
     );
     return $configarray;
 }
 function unitpay_link($params) {
-	global $_LANG;
-	$code = '<form method="post" action="https://'.$params['DOMAIN'].'/pay/'.$params['URL'].'">
+    global $_LANG;
+    $code = '<form method="post" action="https://'.$params['DOMAIN'].'/pay/'.$params['URL'].'">
 		<input type="hidden" name="account" value="'.$params['invoiceid'].'" />
 		<input type="hidden" name="sum" value="'.$params['amount'].'" />
 		<input type="hidden" name="desc"  value="'.$params["description"].'" />
@@ -24,7 +24,8 @@ function unitpay_link($params) {
 		<input type="hidden" name="hideLogo"  value="'.(($params["hideLogo"]=="on")?"true":"false").'" />
 		<input type="submit" value="'.$_LANG["invoicespaynow"].'" />
 		</form>';
-	return $code;
+    return $code;
 }
+
 
 
